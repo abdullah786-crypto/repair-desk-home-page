026f678225f4c26ef4261de26afef1e0d7bf7c68
abdullah786-crypto <abdullahranjha0308@gmail.com>
2025-05-15 14:53:10 +0500

    update freet trail banner

--- src/Components/free-trail-banner.vue
+++ src/Components/free-trail-banner.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="h-85 bg-[#134E4A] w-full flex flex-row">
         <div class="pt-30 pl-13 w-[65%] flex flex-col">
-            <h1 class="text-3xl text-white">Get Started with the #1 Repair Shop POS</h1>
+            <h1 class="text-3xl text-white">Get Started with the #1 Repair Shops POS</h1>
             <button class="bg-[#D4F479] transition hover:scale-110 transition delay-150 duration-300 ease-in-out cursor-pointer mt-6 h-auto p-[11px] font-semibold rounded-3xl w-40 text-lg text-[#134E4A]">Start Free Trail</button>
         </div>
         <div class="w-[35%] mt-5 rounded-[50%]">
